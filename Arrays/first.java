8c13b3f3e5c9b73db151a9b9a4db7de9c8810722
Vidit Raj Singh <rockingvidit860@gmail.com>
2022-06-17 02:03:27 +0530

    add new array section

---
+++ Arrays/first.java
@@ -0,0 +1,12 @@
+package Arrays;
+import java.util.*;
+
+public class first {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s="";
+        System.out.println("Enter name :");
+        s = sc.nextLine();
+        System.out.println("Your name is : " + s);
+    }
+}
