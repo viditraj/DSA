216b29bb59b66e10be0c3baef3d9380902af532e
Vidit Raj Singh <rockingvidit860@gmail.com>
2022-06-20 21:59:17 +0530

    add majorityElement problem

--- Arrays/majorityElement.cpp
+++ Arrays/majorityElement.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 
 //Using HashMap Time Complexity : O(Nlog(N))
-int majorityElement(int ar[] ,int n){
+int majorityElementHM(int ar[] ,int n){
     unordered_map<int,int>major;
     for(int i =0;i<n;i++){
         major[ar[i]]++;
@@ -19,6 +19,37 @@ int majorityElement(int ar[] ,int n){
     return -1;
 }
 
+
+//Time Complexity : O(N)
+int majorityElement(int ar[] ,int n){
+    int candid=ar[0];
+    int count=1;
+    for(int i =1;i<n;i++){
+        if(ar[i]==candid){
+            count++;
+            candid =ar[i];
+        }
+        else{
+            count--;
+        }
+        if(count==0){
+            count=1;
+            candid = ar[i];
+        }
+    }
+    int ans = 0;
+    for(int i =0;i<n;i++){
+        if(ar[i]==candid){
+            ans++;
+        }
+    }
+    if(ans>=n/2){
+        return candid;
+    }
+    return -1;
+
+}
+
 int main(){
     cout<<"Enter the no. of elements :";
     int n;
@@ -28,9 +59,14 @@ int main(){
         cout<<"Enter the "<<i<<"th element :";
         cin>>ar[i];
     }
-    int ans = majorityElement(ar,n);
+    int ans = majorityElementHM(ar,n); //Using Hashmap O(NlogN) time and O(N) space complexity solution
+    int ans2 = majorityElement(ar,n);  //Without using hashmap O(N) and O(1) space complexity solution
     if(ans!=-1)
-        cout<<"Majority Element is : "<<ans;
+        cout<<"Majority Element is : "<<ans<<endl;
     else
-        cout<<"No Majority Element present";
+        cout<<"No Majority Element present"<<endl;
+    if(ans2!=-1)
+        cout<<"Majority Element is : "<<ans2<<endl;
+    else
+        cout<<"No Majority Element present"<<endl;
 }
--- Arrays/tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
-Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no of elements in array :");
-        int n  = sc.nextInt();
-        int ar[] = new int[n];
-        for(int i=0;i<n;i++){
-            System.out.println("Enter the element :");
-            ar[i]=sc.nextInt();
-        }
-        System.out.println("Array after removing duplicates : ");
